614e024470ac73767a0ffed7e9e705d065afb449
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2024-06-16 19:18:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-# Author: naisu-dev <https://github.com/naisu-dev>
-# License: MIT
+import re
 
 from setuptools import setup
-import braipy
 
-setup()
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('./braipy/__init__.py') as f:
+    version = re.findall(r"__version__ = '(.+)'", f.read())[0]
+
+setup(
+    name='braipy',
+    version=version,
+    install_requires=[],
+    description='Tenji',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/naisu-dev/braipy',
+)
