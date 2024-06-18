279ee505a57545dbf2eda0aa0c90b55526d6ead4
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2024-06-18 21:43:31 +0900

    Update main.py

--- braipy/main.py
+++ braipy/main.py
@@ -1,27 +1,28 @@
 import typing
-from .errors import *
 from .dataset.en import en
 
 transdata = {"en": en()}
 
 class Tenji:
     tenji_list = [["1", "3", "5", "7", "9", "b", "d", "f"], ["2", "3", "6", "7", "a", "b", "e", "f"], ["8", "9", "a", "b", "c", "d", "e", "f"], ["4", "5", "6", "7", "c", "d", "e", "f"], ["1", "3"], ["2", "3"]]
-    
+
     def __init__(self, data: typing.List[bool]):
-        self.data = data
-        for i in range(64):
-            if self.data == (list([True if i == "1" else False for i in list((bin(i)[2:]).zfill(6))])[::-1]):
-                self.text = chr(int("28"+(hex(i)[2:]).zfill(2), 16))
-                break
-    
+        if len(data) != 6:
+            raise ValueError("要素数が正しくありません")
+        else:
+            self.data = data
+            for i in range(64):
+                if self.data == (list([True if i == "1" else False for i in list((bin(i)[2:]).zfill(6))])[::-1]):
+                    self.text = chr(int("28"+(hex(i)[2:]).zfill(2), 16))
+                    break
+
     def __repr__(self):
         return self.text
-    
+
     def push(self, data: typing.List[int] = [2, 2, 2, 2, 2, 2]):
         org_data = self.data
         if len(data) != 6:
-            # raise error
-            pass
+            raise ValueError("要素数が正しくありません")
         else:
             for i in range(6):
                 if data[i] == 0:
@@ -39,10 +40,10 @@ class Tenji:
                     # raise error
                     pass
         return Tenji(org_data)
-    
+
     def translate(self, lang):
         pass
-    
+
     @classmethod
     def tenjitext_to_cls(cls, text: str):
         s_16 = hex(ord(text))[2:]
@@ -60,7 +61,7 @@ class Tenji:
         if s_16[2:3] in Tenji.tenji_list[5]: # 右下
             data["6"] = True
         return cls(list(data.values()))
-    
+
     @classmethod
     def text_to_cls(cls, text: str, tenjidict: dict[str, list] = transdata):
         pass
