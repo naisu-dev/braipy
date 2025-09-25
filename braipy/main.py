40cdc61b11182b1dbc6f5a575c9979b98e909e29
ないす <118091656+naisu-dev@users.noreply.github.com>
2025-09-25 16:32:32 +0900

    Update main.py

--- braipy/main.py
+++ braipy/main.py
@@ -1,8 +1,10 @@
-def convbrail(text):
+from enum import IntEnum
+
+def conv_brail(text):
     code_point = ord(text)
 
     if not (0x2800 <= code_point <= 0x28FF):
-        return None
+        raise ValuError("点字じゃない")
 
     offset = code_point - 0x2800
 
@@ -22,3 +24,43 @@ def convbrail(text):
     elif code_point >= 0x2800:
         pass 
     return dots
+
+def create_brail(dots):
+    offset = 0
+    if len(dots) not in [6, 8]:
+        raise ValueError("6 or 8 only")
+
+    if dots[0]: offset |= 0b00000001
+    if dots[1]: offset |= 0b00000010
+    if dots[2]: offset |= 0b00000100
+    if dots[3]: offset |= 0b00001000
+    if dots[4]: offset |= 0b00010000
+    if dots[5]: offset |= 0b00100000
+
+    if len(dots) == 8:
+        if dots[6]: offset |= 0b01000000
+        if dots[7]: offset |= 0b10000000
+
+    code_point = 0x2800 + offset
+    return chr(code_point)
+
+class brailtype(IntEnum):
+    six = 6
+    eight = 8
+
+
+class Brail:
+    def __init__(self, btext: str):
+        self.text = btext
+        self.dots = conv_brail(btext)
+        self.type = brailtype(len(self.dots))
+    
+    @classmethod
+    def dots_from(cls, dots):
+        return cls(create_brail(dots))
+        
+
+my_brail = Brail.dots_from([0, 0, 0, 1, 1, 1, 0, 1])
+print(my_brail.dots)
+
+
