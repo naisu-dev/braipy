2566f0105a992cdcf6c04bef7c32fcad2601f29c
ないす <118091656+naisu-dev@users.noreply.github.com>
2025-09-30 18:15:20 +0900

    Update main.py

--- braipy/main.py
+++ braipy/main.py
@@ -1,34 +1,26 @@
-from enum import IntEnum
-
 def conv_brail(text):
     code_point = ord(text)
 
-    if not (0x2800 <= code_point <= 0x28FF):
+    if not (0x2800 <= code_point <= 0x283f):
         raise ValuError("点字じゃない")
 
     offset = code_point - 0x2800
 
     dots = [
-        (offset & 0b00000001) != 0,  # 点1
-        (offset & 0b00000010) != 0,  # 点2
-        (offset & 0b00000100) != 0,  # 点3
-        (offset & 0b00001000) != 0,  # 点4
-        (offset & 0b00010000) != 0,  # 点5
-        (offset & 0b00100000) != 0,  # 点6
+        (offset & 0b00000001) != 0, 
+        (offset & 0b00000010) != 0, 
+        (offset & 0b00000100) != 0, 
+        (offset & 0b00001000) != 0, 
+        (offset & 0b00010000) != 0, 
+        (offset & 0b00100000) != 0, 
     ]
-
-    if code_point >= 0x2840:
-        dots.append((offset & 0b01000000) != 0)  # 点7
-        dots.append((offset & 0b10000000) != 0)  # 点8
-
-    elif code_point >= 0x2800:
-        pass 
+    
     return dots
 
 def create_brail(dots):
     offset = 0
-    if len(dots) not in [6, 8]:
-        raise ValueError("6 or 8 only")
+    if len(dots) not in [6]:
+        raise ValueError("6 only")
 
     if dots[0]: offset |= 0b00000001
     if dots[1]: offset |= 0b00000010
@@ -37,24 +29,24 @@ def create_brail(dots):
     if dots[4]: offset |= 0b00010000
     if dots[5]: offset |= 0b00100000
 
-    if len(dots) == 8:
-        if dots[6]: offset |= 0b01000000
-        if dots[7]: offset |= 0b10000000
-
     code_point = 0x2800 + offset
     return chr(code_point)
 
-class brailtype(IntEnum):
-    six = 6
-    eight = 8
-
-
-class Brail:
-    def __init__(self, btext: str):
-        self.text = btext
-        self.dots = conv_brail(btext)
-        self.type = brailtype(len(self.dots)) # ここのtypeは形式的なものです！
+class Brail_6:
+    def __init__(self, dots: list):
+        self.text = create_brail(dots)
+        self.dots = dots
     
     @classmethod
-    def dots_from(cls, dots: list[bool]):
-        return cls(create_brail(dots))
+    def text_from(cls, btext: str):
+        return cls(conv_brail(btext))
+
+class LangModel:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        with open(file_path, "r") as f:
+            self.file = f.read()
+
+my_brail = Brail_6([1, 1, 1, 0, 0, 1])
+print(my_brail.text)
+
